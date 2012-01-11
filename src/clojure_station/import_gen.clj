be35f6c607e54012a888f3a9d79b9be84947f2ac
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-11 17:58:58 +0100

    Remove \" as this is more human readable in code and in files generated.

--- src/clojure_station/import_gen.clj
+++ src/clojure_station/import_gen.clj
@@ -26,14 +26,14 @@
 
 (defn- attr-head
   []
-  "\"ACTION\";\"TYPE D'OBJET\";\"CODE CARACTERISTIQUE\";\"SOURCE\";\"NOM\";\"NOM D'AFFICHAGE\";\"UNITE DE MESURE\";\"TYPE D'ATTRIBUT\"")
+  "ACTION;TYPE D'OBJET;CODE CARACTERISTIQUE;SOURCE;NOM;NOM D'AFFICHAGE;UNITE DE MESURE;TYPE D'ATTRIBUT")
 
 (defn- attr-line
   [code]
-  (str "\"CREATE\";\"Attribute\";\"" code "\";\"SID\";\"name-" code "\";\"display-name-" code "\";;\"string\""))
+  (str "CREATE;Attribute;" code ";SID;name-" code ";display-name-" code ";;string"))
 
 (fact "attr-line"
-      (attr-line "titre") => "\"CREATE\";\"Attribute\";\"titre\";\"SID\";\"name-titre\";\"display-name-titre\";;\"string\"")
+      (attr-line "titre") => "CREATE;Attribute;titre;SID;name-titre;display-name-titre;;string")
 
 (defn attr-file
   [attr-codes] (spit-as-lines "/tmp/attr.csv"
@@ -41,21 +41,21 @@
                             (map attr-line attr-codes))))
 
 (defn- model-head
-  [] "\"ACTION\";\"TYPE D'OBJET\";\"CODE DU MODELE\";\"SOURCE\";\"TYPE DE MODELE\";\"NOM\";\"NOM D'AFFICHAGE\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";")
+  [] "ACTION;TYPE D'OBJET;CODE DU MODELE;SOURCE;TYPE DE MODELE;NOM;NOM D'AFFICHAGE;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;MAL;")
 
 (defn- model-line
   [model-code nb attribut-codes]
-  (str "\"CREATE\";\"Model\";\"" model-code "\";\"SID\";\"Product\";\"name-" model-code "\";\"display-name-" model-code "\";"
-       (reduce str (mapcat (fn [attr-code] (str "\"" attr-code "|true|false|\";"))
+  (str "CREATE;Model;" model-code ";SID;Product;name-" model-code ";display-name-" model-code ";"
+       (reduce str (mapcat (fn [attr-code] (str "" attr-code "|true|false|;"))
                            (take nb attribut-codes)))))
 
 (fact "model-line"
       (model-line "ProduitRosier" 0 ["reference" "designation" "foo"]) =>
-      "\"CREATE\";\"Model\";\"ProduitRosier\";\"SID\";\"Product\";\"name-ProduitRosier\";\"display-name-ProduitRosier\";")
+      "CREATE;Model;ProduitRosier;SID;Product;name-ProduitRosier;display-name-ProduitRosier;")
 
 (fact "model-line"
       (model-line "ProduitRosier" 2 ["reference" "designation" "foo"]) =>
-      "\"CREATE\";\"Model\";\"ProduitRosier\";\"SID\";\"Product\";\"name-ProduitRosier\";\"display-name-ProduitRosier\";\"reference|true|false|\";\"designation|true|false|\";")
+      "CREATE;Model;ProduitRosier;SID;Product;name-ProduitRosier;display-name-ProduitRosier;reference|true|false|;designation|true|false|;")
 
 (defn model-file
   [model->attrs] (spit-as-lines
@@ -69,13 +69,13 @@
                              (keys model->attrs)))))
 
 (defn- content-head
-  [] (str "\"ACTION\";\"TYPE D'OBJET\";\"ID CONTENU\";\"SOURCE\";\"CODE DU MODELE\";"
-          (reduce str (repeat 53 "\"CODE DE L'ATTRIBUT\";\"VALEUR DE L'ATTRIBUT\";"))))
+  [] (str "ACTION;TYPE D'OBJET;ID CONTENU;SOURCE;CODE DU MODELE;"
+          (reduce str (repeat 53 "CODE DE L'ATTRIBUT;VALEUR DE L'ATTRIBUT;"))))
 
 (defn- content-line
   [model-code attr-nb attr-codes]
-  (str "\"CREATE\";\"Content\";;\"SITELABO\";\"" model-code "\";"
-       (reduce str (mapcat #(str "\"" % "\";\"" % "-val\";" )
+  (str "CREATE;Content;;SITELABO;" model-code ";"
+       (reduce str (mapcat #(str "" % ";" % "-val;" )
                            (take attr-nb attr-codes)))))
 
 (fact "content-line"
@@ -83,7 +83,7 @@
                     2
                     ["attr1" "attr2"])
       =>
-      "\"CREATE\";\"Content\";;\"SITELABO\";\"ServiceRosier\";\"attr1\";\"attr1-val\";\"attr2\";\"attr2-val\";")
+      "CREATE;Content;;SITELABO;ServiceRosier;attr1;attr1-val;attr2;attr2-val;")
 
 (defn content-file
   [model->attrs content-nb]
