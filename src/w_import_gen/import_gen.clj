149e0817e9f717cfac01614dce66cd664e9662ea
Denis Labaye <denis.labaye@gmail.com>
2012-01-12 08:50:49 +0100

    added doc strings, a few variable renaming

--- src/w_import_gen/import_gen.clj
+++ src/w_import_gen/import_gen.clj
@@ -25,30 +25,30 @@
         (lazy-write-lines filename [1 2])   => nil
         (:out (shell/sh "cat" filename)) => "1\n2\n"))
 
-(defn- attr-head
+(defn- attr-head "The header of an attribute import file"
   []
   "\"ACTION\";\"TYPE D'OBJET\";\"CODE CARACTERISTIQUE\";\"SOURCE\";\"NOM\";\"NOM D'AFFICHAGE\";\"UNITE DE MESURE\";\"TYPE D'ATTRIBUT\"")
 
-(defn- attr-line
+(defn- attr-line "Returns a line for an attribute import file"
   [code]
   (str "\"CREATE\";\"Attribute\";\"" code "\";\"SID\";\"name-" code "\";\"display-name-" code "\";;\"string\""))
 
 (fact "attr-line"
       (attr-line "titre") => "\"CREATE\";\"Attribute\";\"titre\";\"SID\";\"name-titre\";\"display-name-titre\";;\"string\"")
 
-(defn attr-file
+(defn attr-file "Output a file of attribute import file"
   [attr-codes] (lazy-write-lines "/tmp/attr.csv"
                       (cons (attr-head)
                             (map attr-line attr-codes))))
 
-(defn- model-head
+(defn- model-head "Return a header of a import model file"
   [] "\"ACTION\";\"TYPE D'OBJET\";\"CODE DU MODELE\";\"SOURCE\";\"TYPE DE MODELE\";\"NOM\";\"NOM D'AFFICHAGE\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";\"MAL\";")
 
-(defn- model-line
-  [model-code nb attribut-codes]
+(defn- model-line "Return a line of a model import line"
+  [model-code mal-nb attribut-codes]
   (str "\"CREATE\";\"Model\";\"" model-code "\";\"SID\";\"Product\";\"name-" model-code "\";\"display-name-" model-code "\";"
        (reduce str (mapcat (fn [attr-code] (str "\"" attr-code "|true|false|\";"))
-                           (take nb attribut-codes)))))
+                           (take mal-nb attribut-codes)))))
 
 (fact "model-line"
       (model-line "ProduitRosier" 0 ["reference" "designation" "foo"]) =>
@@ -58,7 +58,7 @@
       (model-line "ProduitRosier" 2 ["reference" "designation" "foo"]) =>
       "\"CREATE\";\"Model\";\"ProduitRosier\";\"SID\";\"Product\";\"name-ProduitRosier\";\"display-name-ProduitRosier\";\"reference|true|false|\";\"designation|true|false|\";")
 
-(defn model-file
+(defn model-file "Given a seq of model codes and a seq of model attributes, Write a model import file"
   [models attrs] (lazy-write-lines
                   "/tmp/model.csv"
                   (cons (model-head)
@@ -68,11 +68,11 @@
                                            attrs ))
                              models))))
 
-(defn- content-head
+(defn- content-head "Return the header of a content import file"
   [] (str "\"ACTION\";\"TYPE D'OBJET\";\"ID CONTENU\";\"SOURCE\";\"CODE DU MODELE\";"
           (reduce str (repeat 53 "\"CODE DE L'ATTRIBUT\";\"VALEUR DE L'ATTRIBUT\""))))
 
-(defn- content-line
+(defn- content-line "Given a model code, a number of attribute and a seq of all attributes codes, return a line of content import file"
   [model-code attr-nb attr-codes]
   (str "\"CREATE\";\"Content\";;\"SITELABO\";\"" model-code "\""
        (reduce str (mapcat #(str ";\"" % "\";\"" % "-val\"" )
@@ -85,7 +85,7 @@
       =>
       "\"CREATE\";\"Content\";;\"SITELABO\";\"ServiceRosier\";\"attr1\";\"attr1-val\";\"attr2\";\"attr2-val\"")
 
-(defn content-file
+(defn content-file "Given a seq of model codes, a seq of attributes code an a number of content to produce, write a content import file"
   [models attrs content-nb]
   (lazy-write-lines "/tmp/content.csv"
                     (cons (content-head)
@@ -95,7 +95,7 @@
                                         (content-line model-code (count attrs) attrs))
                                       models))))))
 
-(defn make-meta "Create a map of wikeo meta data"
+(defn make-meta "Given a number of models and an number of attributes per model, return a seq of model codes and a seq of attribute codes"
   [{:keys [model-nb attrs-per-model-nb]}]
   {:attributes (map #(str "a" (inc %)) (range attrs-per-model-nb))
    :models     (map #(str "m" (inc %)) (range model-nb))})
@@ -105,7 +105,7 @@
               :attrs-per-model-nb 3}) => {:attributes ["a1" "a2" "a3"]
                                           :models     ["m1" "m2"]})
 
-(defn all-file
+(defn all-file "Given a seq of attribute codes and a seq of model code, 3 files import files: one for attribute, one for models and one for content, each models has all the attributes and each content has all the attributes."
   [args]
   (let [{:keys [attributes models]}
         (make-meta args)]
