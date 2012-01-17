0afd68767381b096cb832c1a34d49fb2324c09fd
Denis Labaye <denis.labaye@gmail.com>
2012-01-17 14:22:26 +0100

    model-count-mals

--- project.clj
+++ project.clj
@@ -1,7 +1,8 @@
 (defproject w-import-gen "1.0.0-SNAPSHOT"
   :description "FIXME: write description"
   :dependencies [[org.clojure/clojure         "1.3.0"]
-                 [org.clojure/tools.cli "0.2.1"]]
+                 [org.clojure/tools.cli "0.2.1"]
+                 [clojure-csv/clojure-csv "1.3.2"]]
   :dev-dependencies [[midje      "1.3.1"]]
   #_(:main w-import-gen.io.main))
 
--- src/w_import_gen/analyse.clj
+++ src/w_import_gen/analyse.clj
@@ -1,12 +1,15 @@
 (ns ^{:doc "Analyse a content file fo find the distribution of the MALs per content"}
   w-import-gen.io.analyse
-  (:use [w-import-gen.core :only [attributes models contents make-meta]]
+  (:use [w-import-gen.core    :only [attributes models contents make-meta]]
+        [w-import-gen.io.util :only [read-lines]]
         [midje.sweet]
         [clojure.tools.cli]
-        [clojure.repl
-         :only [doc]]))
+        [clojure.repl :only [doc]]
+        [clojure-csv.core :only [parse-csv]]))
 
 (println "--------- BEGIN OF ANALYSE  ----------" (java.util.Date.))
+
+(unfinished )
 
 (def nb-items-model-before-mals 7)
 
@@ -17,8 +20,29 @@
       (model-count-line 1 ["stuff" "mal1" "mal2"]) => 2
       (model-count-line 1 ["stuff"  "mal1" "mal2" ""]) => 2)
 
+(defn model-count-mals
+  [lines] (reduce (fn [r l]
+                    (+ r (model-count-line nb-items-model-before-mals
+                                           (parse-csv l))))
+                  0
+                  (rest lines)))
+
+(fact "model-count-mals"
+      (model-count-mals [:h :l1 :l2]) => 3
+      (provided
+       (parse-csv :l1) => :cols1
+       (parse-csv :l2) => :cols2
+       (model-count-line nb-items-model-before-mals :cols1) => 1
+       (model-count-line nb-items-model-before-mals :cols2) => 2))
+
 (defn model-count-mal
-  [f] (println "Counting mal from " f))
+  [f] (model-count-mals (read-lines f)))
+
+(fact "model-count-mal"
+      (model-count-mal :filename) => :res
+      (provided
+       (read-lines :filename)    => :lines
+       (model-count-mals :lines) => :res))
 
 (defn -main [& args]
   (let [[options args banner :as opts]
