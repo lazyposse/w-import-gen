51a5043c995055e2e0da8f0184a9fd6fa472a003
Denis Labaye <denis.labaye@gmail.com>
2012-01-17 07:33:25 +0100

    temporaraly disabling main class generation, as it seems to really slow down emacs (swank) startup

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[org.clojure/clojure         "1.3.0"]
                  [org.clojure/tools.cli "0.2.1"]]
   :dev-dependencies [[midje      "1.3.1"]]
-  :main w-import-gen.io.main)
+  #_(:main w-import-gen.io.main))
 
 
