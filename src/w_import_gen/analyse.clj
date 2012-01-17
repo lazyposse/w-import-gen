9a2833cac6420b59620bf1235da6c97c3b6ae7e3
Denis Labaye <denis.labaye@gmail.com>
2012-01-17 13:45:04 +0100

    initiated the entry point for analyse

--- src/w_import_gen/analyse.clj
+++ src/w_import_gen/analyse.clj
@@ -3,12 +3,8 @@
   (:use [w-import-gen.core :only [attributes models contents make-meta]]
         [midje.sweet]
         [clojure.tools.cli]
-        [clojure.repl :only [doc]]
-        )
-  (:require [clojure.java.shell :as shell]
-            [clojure.java.io :as io]
-            [clojure-csv.core :as csv]
-            ))
+        [clojure.repl
+         :only [doc]]))
 
 (println "--------- BEGIN OF ANALYSE  ----------" (java.util.Date.))
 
@@ -21,6 +17,20 @@
       (model-count-line 1 ["stuff" "mal1" "mal2"]) => 2
       (model-count-line 1 ["stuff"  "mal1" "mal2" ""]) => 2)
 
+(defn model-count-mal
+  [f] (println "Counting mal from " f))
+
+(defn -main [& args]
+  (let [[options args banner :as opts]
+        (cli args
+             ["-h" "--help"           "Show help" :default false :flag true]
+             ["-f" "--model-filename" "CSV file to read"])]
+
+    (when (options :help)
+      (println banner)
+      (System/exit 0))
+    ;; generates the import files
+    (model-count-mal (options :model-filename))))
 
 
 (println "--------- END OF ANALYSE  ----------" (java.util.Date.))
