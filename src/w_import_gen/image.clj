e0453d7cb45a47613d24008a46e6526a899388b6
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2012-01-26 15:24:12 +0100

    read-content!

--- src/w_import_gen/image.clj
+++ src/w_import_gen/image.clj
@@ -1,10 +1,11 @@
 (ns ^{:doc "Remove unecessary images"}
   w-import-gen.image
   (:use [midje.sweet]
-        [clojure-csv.core :only [parse-csv]])
-  (:require [  w-import-gen.io.util :as u]))
+        [clojure-csv.core :only [parse-csv]]
+        [clojure.repl :only [doc]])
+  (:require [w-import-gen.io.util :as u]))
 
-(unfinished filter-img read-content! )
+(unfinished filter-img )
 
 (defn get-img-id [l]
   (nth (.split l ";") 2))
@@ -21,12 +22,21 @@
        (u/read-lines :img-file) => [:line]
        (get-img-id :line) => :id))
 
+(defn read-content!
+  [content-file]
+  (set (mapcat #(get-img-id %) (u/read-lines content-file))))
 
+(fact "read-content!"
+  (read-content! :content-file) => #{:img-id1 :img-id2 :img-id3}
+  (provided
+    (u/read-lines :content-file) => [:line1 :line2]
+    (get-img-id :line1) => #{:img-id1 :img-id2}
+    (get-img-id :line2) => #{:img-id3}))
 
 (defn filter-img!
   [img-file content-file out-file] (let [img (read-img! img-file)
-                                    content (read-content! content-file)]
-                                (u/lazy-write-lines out-file (filter-img img content))))
+                                         content (read-content! content-file)]
+                                     (u/lazy-write-lines out-file (filter-img img content))))
 
 (fact
  (filter-img! :img-file :content-file :out-file) => nil
