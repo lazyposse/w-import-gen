a53e6ea83d4b90548c17a7d863772e8b6d6240bc
Denis Labaye <labaye.d@sfeir.com>
2012-01-26 13:52:03 +0100

    filter-image: in progress

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :dependencies [[org.clojure/clojure         "1.3.0"]
                  [org.clojure/tools.cli "0.2.1"]
                  [clojure-csv/clojure-csv "1.3.2"]]
-  :dev-dependencies [[midje      "1.3.1"]]
-  #_(:main w-import-gen.io.main))
+  :dev-dependencies [[midje "1.3.1"]
+                     [com.intelie/lazytest "1.0.0-SNAPSHOT" :exclusions [swank-clojure]]])
 
 
